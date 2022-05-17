62d8b464396e78a557c51cc37c4a219d2a5a13e8
Marcin Franikowski <marcin.franikowski@silksh.pl>
2022-05-17 14:01:35 +0200

    First commit

---
+++ task_run_definition.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket_object" "task_run_definition" {
+  bucket  = var.task_run_definition_bucket
+  key     = var.task_run_definition_file
+  content = <<EOF
+{
+    "cluster": "${var.cluster_id}",
+    "taskDefinition": "${aws_ecs_task_definition.this.arn}",
+    "startedBy": "deployment-script",
+    "group": "${var.service_name}-${var.env_name}",
+    "launchType": "EC2"
+}
+EOF
+}
